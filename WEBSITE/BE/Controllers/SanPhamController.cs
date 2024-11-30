f6b935866eaddfaa33aa5d83540d93d2323c8765
huynhhuan <huynhbahuan@gmail.com>
2024-11-30 22:13:20 +0700

    code moi

---
+++ WEBSITE/BE/Controllers/SanPhamController.cs
@@ -0,0 +1,23 @@
+﻿using BE.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SanPhamController : Controller
+    {
+        private readonly db_websitebanhangContext _context;
+        public SanPhamController(db_websitebanhangContext context) 
+        {
+            _context = context;
+        }
+        [HttpGet(Name = "sanpham")]
+        public async Task <IEnumerable<Sanpham>> Getsanpham()
+        {
+            return await _context.Sanphams.ToListAsync();
+        }
+
+    }
+}
