5773e233a87708f6be3d223f3b6c6980305fc32a
Huanhuynh24 <huanhuynh2402@gmail.com>
2024-12-02 11:53:28 +0700

    update

---
+++ WEBSITE/BE/Repository/SanphamH.cs
@@ -0,0 +1,41 @@
+﻿using BE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE.Repository
+{
+    public class SanphamH
+    {
+        private readonly db_websitebanhangContext _context;
+
+        public SanphamH(db_websitebanhangContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Object>> getAllSanphams()
+        {
+
+            var sanphams = await _context.Sanphams
+                .Include(sp => sp.MaNhanNavigation)
+                .Include(sp => sp.MaKhuyenmaiNavigation)
+                .Include(sp => sp.Chitietsanphams)
+                .ThenInclude(ct => ct.MaMauNavigation)
+                .Select(sp =>  new
+                {
+                    sp.TenSanpham,
+                    sp.MoTa,
+                    Chitietsanpham = sp.Chitietsanphams.Select(ct => new
+                    {
+                        ct.HinhAnh,
+                        ct.SoLuongTon,
+                        ct.Gia,
+                        ct.MaMauNavigation.TenMau
+                    }),
+                    Nhanhieu = sp.MaNhanNavigation.TenNhan,
+                    phantramkhuyenmai = sp.MaKhuyenmaiNavigation.PhanTram,
+                }).ToListAsync();
+
+            return sanphams;
+        } 
+    }
+}
