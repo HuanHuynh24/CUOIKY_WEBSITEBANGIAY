77dd2c6d07c67411dfae43bb2b15fe48b2956e4d
huynhhuan <huynhbahuan@gmail.com>
2024-12-03 21:47:55 +0700

    test

--- WEBSITE/BE/obj/Debug/net6.0/BE.AssemblyInfo.cs
+++ WEBSITE/BE/obj/Debug/net6.0/BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+588e825b9c5a9d79d3b0841447e66b900cfa2ef2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e32e42fcff7ae0882c911a2f790806d8b4b0252")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
