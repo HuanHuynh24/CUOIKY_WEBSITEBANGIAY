27655e22a3aab6465913bb783135c7889958d9b3
vikhuong24 <vikhuong13@gmail.com>
2024-12-03 22:30:11 +0700

    vikhuong0312l3

--- WEBSITE/BE/obj/Debug/net6.0/BE.AssemblyInfo.cs
+++ WEBSITE/BE/obj/Debug/net6.0/BE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8651cd2bfe6d3d4ac6004f7b752506491ff4dce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61006be6d8dc2890b9040564979e4fe715782bb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
